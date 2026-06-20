75e38f91ebf8efce26228c6924cbcc87847302b8
Chang Min LEE <nilpotential@proton.me>
2026-06-21 07:41:33 +0900

    do not watch assignment

--- src/directives/scope.ts
+++ src/directives/scope.ts
@@ -2,11 +2,7 @@ import type { Directive } from "@wyvernjs/core";
 
 export const scope: Directive = {
   name: "w-scope",
-  handler({ el, evaluate, scope, expression, watch }) {
-    watch(
-      () => evaluate(scope, expression!, el),
-      (data) => Object.assign(scope.value, data),
-      { immediate: true },
-    );
+  handler({ el, evaluate, scope, expression }) {
+    Object.assign(scope.value, evaluate(scope, expression!, el));
   },
 };
