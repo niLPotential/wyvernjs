922c7ee196769088e85c757b5da00e89145f696d
Chang Min LEE <nilpotential@proton.me>
2026-06-21 21:45:33 +0900

    collapse scope

--- src/core/evaluate.ts
+++ src/core/evaluate.ts
@@ -1,8 +1,36 @@
-import type { Ref } from "@vue/reactivity";
+import type { Scope } from "./types.ts";
 
-export function evaluate(scope: Ref, exp: string, el: Element) {
+export function evaluate(scope: Scope, exp: string, el: Element) {
   // deno-lint-ignore no-explicit-any
-  return toFunction(exp).bind(null, scope.value, el) as () => any;
+  return toFunction(exp).bind(null, collapse(scope), el) as () => any;
+}
+
+function collapse(scope: Scope) {
+  return new Proxy(scope, {
+    ownKeys(target) {
+      return target.value.flatMap((o) => Object.keys(o));
+    },
+    has(target, name) {
+      return target.value.some((o) =>
+        Object.hasOwn(o, name) || Reflect.has(o, name)
+      );
+    },
+    get(target, name, receiver) {
+      return Reflect.get(
+        target.value.findLast((o) => Reflect.has(o, name)) ?? {},
+        name,
+        receiver,
+      );
+    },
+    set(target, name, receiver) {
+      return Reflect.set(
+        target.value.findLast((o) => Reflect.has(o, name)) ??
+          target.value[target.value.length - 1],
+        name,
+        receiver,
+      );
+    },
+  });
 }
 
 // From https://github.com/vuejs/petite-vue/blob/main/src/eval.ts
